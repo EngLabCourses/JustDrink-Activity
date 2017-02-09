4cce26da271e00523a1dd60e7c337874db508eb0
spawn150 <spawn150@gmail.com>
2017-02-09 14:34:18 +0100

    gestito settaggio del nome nel layout

--- app/src/main/java/it/englab/androidcourse/justdrink/ui/home/MainActivity.java
+++ app/src/main/java/it/englab/androidcourse/justdrink/ui/home/MainActivity.java
@@ -1,18 +1,33 @@
 package it.englab.androidcourse.justdrink.ui.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 import it.englab.androidcourse.justdrink.R;
 import it.englab.androidcourse.justdrink.model.Drink;
+import it.englab.androidcourse.justdrink.model.DrinkFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView textViewDrinkName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewDrinkName = (TextView) findViewById(R.id.drink_name);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Drink randomDrink = DrinkFactory.getRandomDrink();
+        Log.d(TAG, "Random Drink info: " + randomDrink);
+        textViewDrinkName.setText(randomDrink.getStrDrink());
     }
 }

e29fe327607ce246bf71353ac48fabec5f468758
spawn150 <spawn150@gmail.com>
2017-02-09 12:50:46 +0100

    aggiunto ignore su vcs.xml

--- app/src/main/java/it/englab/androidcourse/justdrink/model/DrinkFactory.java
+++ app/src/main/java/it/englab/androidcourse/justdrink/model/DrinkFactory.java
@@ -7,9 +7,7 @@ package it.englab.androidcourse.justdrink.model;
 public class DrinkFactory {
 /*
     {
-        "strDrink": "Cosmopolitan Martini",
-            "strDrinkThumb": "http://www.thecocktaildb.com/images/media/drink/upxxpq1439907580.jpg",
-            "idDrink": "14133"
+
     },
     {
         "strDrink": "Dark Caipirinha",
@@ -41,12 +39,18 @@ public class DrinkFactory {
             "strDrinkThumb": "http://www.thecocktaildb.com/images/media/drink/rxtqps1478251029.jpg",
             "idDrink": "13427"
     }*/
-
+    
     private static final Drink[] drinks = {
             new Drink.Builder()
                     .idDrink("14107").strDrink("Absolut Summertime").strCategory("Cocktail")
                     .strGlass("Collins glass").strInstructions("Add all ingredients except lemon to shaker filled with ice. Cover and shake vigorously. Strain contents into ice filled collins glass. Garnish with lemon.")
                     .ingredients(new String[]{"Absolut Citron", "Sweet and sour", "Sprite", "Soda water", "Lemon"})
-                    .measures(new String[]{"1 1/2 oz ", "3/4 oz ", "1/2 oz ", "3 oz ", "1 slice "}).build()
+                    .measures(new String[]{"1 1/2 oz ", "3/4 oz ", "1/2 oz ", "3 oz ", "1 slice "}).build(),
+            new Drink.Builder()
+                    .idDrink("14133").strDrink("Cosmopolitan Martini").strCategory("Cocktail")
+                    .strGlass("vote").strInstructions("Pour all ingredients in mixing glass half filled with ice, shake and strain into chilled Martini glass.")
+                    .ingredients(new String[]{"Cointreau", "Vodka", "Lime", "Cranberry juice"})
+                    .measures(new String[]{"1/2 oz ", "1 oz ", "Juice of 1/2 ", "1 splash "}).build()
+
     };
 }

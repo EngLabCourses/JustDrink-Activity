b6fdb233c06b9c4031010c03c1bfd58fe2f36c7a
spawn150 <spawn150@gmail.com>
2017-02-09 14:14:03 +0100

    aggiunto drink

--- app/src/main/java/it/englab/androidcourse/justdrink/model/DrinkFactory.java
+++ app/src/main/java/it/englab/androidcourse/justdrink/model/DrinkFactory.java
@@ -31,11 +31,7 @@ public class DrinkFactory {
             "strDrinkThumb": "http://www.thecocktaildb.com/images/media/drink/qvuyqw1441208955.jpg",
             "idDrink": "13936"
     },
-    {
-        "strDrink": "Mojito",
-            "strDrinkThumb": "http://www.thecocktaildb.com/images/media/drink/rxtqps1478251029.jpg",
-            "idDrink": "13427"
-    }*/
+    */
 
     private static final Drink[] drinks = {
             new Drink.Builder()
@@ -57,7 +53,12 @@ public class DrinkFactory {
                     .idDrink("17177").strDrink("Dark Caipirinha").strCategory("Cocktail")
                     .strGlass("Highball glass").strInstructions("Muddle the sugar into the lime wedges in an old-fashioned glass.\\r\\nFill the glass with ice cubes.\\r\\nPour the cachaca into the glass.\\r\\nStir well.")
                     .ingredients(new String[]{"demerara Sugar", "Lime", "Cachaca"})
-                    .measures(new String[]{"2 tsp ", "1 ", "2 1/2 oz"}).build()
+                    .measures(new String[]{"2 tsp ", "1 ", "2 1/2 oz"}).build(),
+            new Drink.Builder()
+                    .idDrink("13427").strDrink("Mojito").strCategory("Cocktail")
+                    .strGlass("Highball glass").strInstructions("Lightly muddle the mint and sugar with a splash of soda water in a mixing glass until the sugar dissolve and you smell the mint. Squeeze the lime into the glass, add rum and shake with ice. Strain over cracked ice in a highball glass. Top with soda water, garnish with mint sprig and serve.")
+                    .ingredients(new String[]{"Light rum", "Lime", "Sugar", "Mint", "Soda water"})
+                    .measures(new String[]{"2-3 oz ", "Juice of 1 ", "2 tsp ", "2-4"}).build()
 
     };
 }

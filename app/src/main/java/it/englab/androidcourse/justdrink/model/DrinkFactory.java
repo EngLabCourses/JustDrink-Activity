4da09102dea585824e7da1a49b7d94688ebff69d
spawn150 <spawn150@gmail.com>
2017-02-09 14:33:53 +0100

    eliminato Drink

--- app/src/main/java/it/englab/androidcourse/justdrink/model/DrinkFactory.java
+++ app/src/main/java/it/englab/androidcourse/justdrink/model/DrinkFactory.java
@@ -24,11 +24,6 @@ public class DrinkFactory {
                     .ingredients(new String[]{"demerara Sugar", "Lime", "Cachaca"})
                     .measures(new String[]{"2 tsp ", "1 ", "2 1/2 oz"}).build(),
             new Drink.Builder()
-                    .idDrink("17177").strDrink("Dark Caipirinha").strCategory("Cocktail")
-                    .strGlass("Highball glass").strInstructions("Muddle the sugar into the lime wedges in an old-fashioned glass.\\r\\nFill the glass with ice cubes.\\r\\nPour the cachaca into the glass.\\r\\nStir well.")
-                    .ingredients(new String[]{"demerara Sugar", "Lime", "Cachaca"})
-                    .measures(new String[]{"2 tsp ", "1 ", "2 1/2 oz"}).build(),
-            new Drink.Builder()
                     .idDrink("13427").strDrink("Mojito").strCategory("Cocktail")
                     .strGlass("Highball glass").strInstructions("Lightly muddle the mint and sugar with a splash of soda water in a mixing glass until the sugar dissolve and you smell the mint. Squeeze the lime into the glass, add rum and shake with ice. Strain over cracked ice in a highball glass. Top with soda water, garnish with mint sprig and serve.")
                     .ingredients(new String[]{"Light rum", "Lime", "Sugar", "Mint", "Soda water"})

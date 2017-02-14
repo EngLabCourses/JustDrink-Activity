613e7c621d748723f1b8d3b202ae0bcc20166de2
spawn150 <spawn150@gmail.com>
2017-02-14 18:57:38 +0100

    creata nuova detail for result

---
+++ app/src/main/java/it/englab/androidcourse/justdrink/ui/detail/DetailActivityForResult.java
@@ -0,0 +1,12 @@
+package it.englab.androidcourse.justdrink.ui.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivityForResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

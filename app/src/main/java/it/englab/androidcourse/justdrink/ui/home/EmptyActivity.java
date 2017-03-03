fba60fba284fd775186ea56de48e9df6bc6ff312
spawn150 <spawn150@gmail.com>
2017-03-03 15:00:35 +0100

    aggiunta Activity vuota

---
+++ app/src/main/java/it/englab/androidcourse/justdrink/ui/home/EmptyActivity.java
@@ -0,0 +1,10 @@
+package it.englab.androidcourse.justdrink.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by spawn on 03/03/17.
+ */
+
+public class EmptyActivity extends AppCompatActivity {
+}
